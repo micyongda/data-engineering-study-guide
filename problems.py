dad3a1b6fa9e9bdfc8fad1ad7af231a0d5db1ef7
micyongda <57813967+micyongda@users.noreply.github.com>
2019-12-21 12:30:07 -0600

    create python practice problems

---
+++ problems.py
@@ -0,0 +1,5 @@
+# 1. Write a function in Python to check if a number is prime.
+
+# 2. Write a function in Python to check if a sequence is a Palindrome.
+
+# 3. Write a function that takes a roman numeral input str and converts it to an int.
